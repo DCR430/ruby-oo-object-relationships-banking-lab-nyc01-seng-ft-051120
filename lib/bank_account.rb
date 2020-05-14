4f0076c92f34c64340b58ebeca40035dfe708121
DCR430 <daniel.c.reyes30@gmail.com>
2020-05-14 14:29:04 -0400

    bank lab 51420

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+    attr_reader :name
+    
+    def initialize (name ,balance = 1000, status = "open")
+        @bank_account = "Your Bank Account is"
+        @name = name
+        @balance = balance
+        @status = status
+
+    end
+     
+    def deposit (money)
+        @balance += money
+
+    end
+
+    def display_balance
+        @balance 
+        p "Your balance is $#{@balance}."
+    end
+
+    def valid?
+         @status == "open" && @balance > 0
+           
+    end
+
+    def close_account
+        @status = "closed"
+    
+    end
+
+
+
+
+
+
+
 end
